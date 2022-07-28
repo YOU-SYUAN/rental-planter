f72b29ba9772b4b3e02c3656e895e276d57e1e8d
An-HS <zxc5894265@gmail.com>
2022-07-28 18:11:14 +0800

    Add break point on Login page, some problems remain unsovled.

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -6,8 +6,19 @@ function App() {
   return (
     <div className="App">
       {/* style={{ backgroundImage: `url(${Background})` }} */}
-      <div>
-        <img src={require("../assets/homeIMG.png")} class="bg-cover"></img>
+      <div class="relative">
+        <img
+          src={require("../assets/homeIMG.png")}
+          class="object-cover h-[1280px] absolute"
+        />
+        <div class="top-[100px] w-full fixed h-[93px]"></div>
+        <p class="mt-[99.5px] w-full text-center fixed text-[44px] text-white font-['Nova_Flat'] md:text-[64px] md:mt-[188px] lg:text-[120px] lg:mt-[100px]">
+          Rental Planter
+        </p>
+        <p class="mt-[176.5px] text-center fixed text-[16px] w-full text-white tracking-[.40em] md:text-[20px] md:mt-[309px] lg:text-[36px] lg:mt-[243px]">
+          用心照顧你的植物
+        </p>
+
         <div
           id="authentication-modal"
           tabindex="-1"
@@ -16,8 +27,10 @@ function App() {
           role="dialog"
         >
           <div class="relative p-4 w-full max-w-md h-full md:h-auto">
-            <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
-              {/* <button
+            <div class="relative bg-white rounded-lg mt-[240px] md:mt=[400px] lg:mt-[393px] shadow dark:bg-gray-700 py-12">
+              <p class="text-[24px] text-center font-semibold">Monospace VIP</p>
+              <div class="relative mt-[48px]">
+                {/* <button
                 type="button"
                 class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white"
                 data-modal-toggle="authentication-modal"
@@ -37,44 +50,44 @@ function App() {
                 </svg>
                 <span class="sr-only">Close modal</span>
               </button> */}
-              <div class="py-6 px-6 lg:px-8">
-                <h3 class="mb-4 text-xl font-medium text-gray-900 dark:text-white">
-                  Sign in to our platform
-                </h3>
-                <form class="space-y-6" action="#">
-                  <div>
-                    <label
-                      for="email"
-                      class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-                    >
-                      Your email
-                    </label>
-                    <input
-                      type="email"
-                      name="email"
-                      id="email"
-                      class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
-                      placeholder="name@company.com"
-                      required=""
-                    />
-                  </div>
-                  <div>
-                    <label
-                      for="password"
-                      class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-                    >
-                      Your password
-                    </label>
-                    <input
-                      type="password"
-                      name="password"
-                      id="password"
-                      placeholder="••••••••"
-                      class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
-                      required=""
-                    />
-                  </div>
-                  <div class="flex justify-between">
+                <div class="py-6 px-6 lg:px-8">
+                  <h3 class="mb-4 text-xl font-medium text-gray-900 dark:text-white">
+                    登入會員植物管理系統
+                  </h3>
+                  <form class="space-y-6" action="#">
+                    <div>
+                      <label
+                        for="email"
+                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                      >
+                        Your email
+                      </label>
+                      <input
+                        type="email"
+                        name="email"
+                        id="email"
+                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+                        placeholder="name@company.com"
+                        required=""
+                      />
+                    </div>
+                    <div>
+                      <label
+                        for="password"
+                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                      >
+                        Your password
+                      </label>
+                      <input
+                        type="password"
+                        name="password"
+                        id="password"
+                        placeholder="••••••••"
+                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+                        required=""
+                      />
+                    </div>
+                    {/* <div class="flex justify-between">
                     <div class="flex items-start">
                       <div class="flex items-center h-5">
                         <input
@@ -98,14 +111,14 @@ function App() {
                     >
                       Lost Password?
                     </a>
-                  </div>
-                  <button
-                    type="submit"
-                    class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                  >
-                    Login to your account
-                  </button>
-                  <div class="text-sm font-medium text-gray-500 dark:text-gray-300">
+                  </div> */}
+                    <button
+                      type="submit"
+                      class="w-full text-white bg-[#519E75] hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                    >
+                      Login to your account
+                    </button>
+                    {/* <div class="text-sm font-medium text-gray-500 dark:text-gray-300">
                     Not registered?{" "}
                     <a
                       href="#"
@@ -113,8 +126,9 @@ function App() {
                     >
                       Create account
                     </a>
-                  </div>
-                </form>
+                  </div> */}
+                  </form>
+                </div>
               </div>
             </div>
           </div>
