c16c7e76ad747b76f9ab7ad2d2f5f6ee9c22b02e
An-HS <zxc5894265@gmail.com>
2022-08-15 15:32:03 +0800

    feat: :art: Add break point for tablet and phone.

--- src/pages/ResetPwd.js
+++ src/pages/ResetPwd.js
@@ -3,45 +3,48 @@ import plantIMG from "../assets/bg2.png";
 function Register() {
   return (
     <div
-      class="relative bg-cover flex justify-center items-center"
+      class="relative bg-cover flex justify-center items-center tablet:flex-col phone:flex-col"
       style={{ backgroundImage: `url(${Background})`, height: "100vh" }}
     >
-      <div class="rounded-3xl bg-white flex flex-row tablet:flex-col h-3/5 w-4/5">
+      <div class="rounded-3xl bg-white flex flex-row h-3/5 w-4/5 tablet:flex-col tablet:w-3/5 phone:flex-col phone:w-4/5">
         {/* <div class=""> */}
-        <img src={plantIMG} class="rounded-l-3xl object-none w-2/5"></img>
+        <img
+          src={plantIMG}
+          class="desktop:rounded-l-3xl object-cover w-2/5 tablet:w-full tablet:h-2/5 tablet:rounded-t-3xl phone:w-full phone:h-2/5 phone:rounded-t-3xl"
+        ></img>
         {/* </div> */}
         {/* <div class="flex justify-center"> */}
-        <div class="flex flex-wrap flex-col py-12 w-3/5 space-y-12">
+        <div class="flex flex-wrap flex-row m-auto py-12 w-3/5 space-y-12 overflow-y-auto tablet:w-full phone:w-full">
           {/* <div class="w-full justify-center"> */}
-          <div>
-            <h1 class="text-center text-[40px] tablet:pt-[20px] tablet:text-[20px] font-Nova_Flat font-normal">
+          <div class="w-full">
+            <h1 class="text-center text-[40px] font-Nova_Flat font-normal tablet:text-[32px] phone:text-[24px]">
               忘記密碼
             </h1>
-            <h2 class="text-[20px] text-center text-[#929292] tracking-widest font-normal">
+            <h2 class="text-[20px] text-center text-[#929292] tracking-widest font-normal tablet:text-[20px] phone:text-[14px]">
               Rental Planter
             </h2>
           </div>
-          <div class="w-full flex flex-col items-center">
+          <div class="w-full flex flex-col items-center tablet:px-10 phone:px-10">
             <label
               for="email"
-              class="block mb-2 text-sm font-medium text-black w-1/2"
+              class="block mb-2 text-sm font-medium text-black w-1/2 tablet:w-full phone:w-full"
             >
               電子郵件
             </label>
             <input
               type="email"
               id="email"
-              class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg w-1/2 h-[42px]"
+              class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg w-1/2 h-[42px] tablet:w-full phone:w-full"
               placeholder="name@flowbite.com"
               required
             />
           </div>
 
-          <div class="flex flex-col w-full space-y-2 items-center">
-            <button class="w-1/2 h-[42px] text-[14px] bg-[#519E75] text-white rounded-lg">
+          <div class="flex flex-col w-full space-y-2 items-center tablet:px-10 phone:px-10">
+            <button class="w-1/2 h-[42px] text-[14px] bg-[#519E75] text-white rounded-lg tablet:w-full phone:w-full">
               確認
             </button>
-            <button class="w-1/2 h-[42px] text-[14px] bg-[#929292] text-white rounded-lg">
+            <button class="w-1/2 h-[42px] text-[14px] bg-[#929292] text-white rounded-lg tablet:w-full phone:w-full">
               取消
             </button>
           </div>
