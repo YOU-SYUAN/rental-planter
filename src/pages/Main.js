f063b913dc8b0e1848a99e5f98c55de470e18947
jimchen5209 <jimchen5209@gmail.com>
2022-10-18 17:59:20 +0800

    fix: footer rwd

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -364,18 +364,18 @@ const Main = () => {
             ))}
         </div>
       </div>
-      <div class="flex justify-center bg-[#F9FAFB]">
-        <div class="mt-[60px] flex flex-col tablet:mt-10">
-          <h1 class="font-extrabold text-[36px] tablet:text-[24px] phone:text-[20px]">
+      <div class="w-full flex justify-center bg-[#F9FAFB] desktop:py-24 py-12">
+        <div class="flex flex-col items-center gap-6">
+          <h1 class="font-extrabold desktop:text-[36px] tablet:text-[24px] text-[20px] tracking-widest">
             現在就租借你的盆器
           </h1>
-          <div class="text-[#6B7280] mt-6 text-center text-[20px] tablet-[16px] phone:text-[14px]">
+          <div class="text-[#6B7280] desktop:text-[20px] tablet:text-[16px] text-[14px]">
             用心愛護你的植物
           </div>
-          <div class="text-center">
+          <div class="flex justify-center">
             <button
               onClick={showModal}
-              class="bg-[#519E75] w-[136px] h-[60px] text-[24px] text-white rounded-lg mt-6 mb-24 tablet:mb-[45px] tablet:mt-[45px] tablet:w-[104px] tablet:h-[48px] tablet:text-[16px] phone:w-[88px] phone:h-[42px] phone:text-[18px]"
+              class="bg-[#519E75] text-white rounded-lg desktop:text-[16px] tablet:text-[16px] text-[12px] px-5 py-3"
             >
               立即登記
             </button>
