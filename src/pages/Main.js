b1437041f8cb96b23dd479899761f8dc1e9ef05f
jimchen5209 <jimchen5209@gmail.com>
2022-10-19 10:16:49 +0800

    refactor: Move components that only use once back to main

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,6 +1,6 @@
 import background from "../assets/work.png";
-import Intro from "../components/main/Intro";
-import State from "../components/main/State";
+import plant from "../assets/plant.png";
+import StatePlant from "../components/main/StatePlant";
 import logo from "../assets/logo.png";
 import Showplant from "../components/main/Showplant";
 import lamu from "../assets/img1.png";
@@ -343,28 +343,74 @@ const Main = () => {
           </div>
         </div>
       </div>
+      {/* Intro */}
       <div id="introduce" class="w-full flex justify-center">
-        <Intro></Intro>
+        <div class="grid grid-cols-2 w-full desktop:max-w-[1560px] tablet:max-w-[768px] max-w-[375px] desktop:p-20 p-4 desktop:gap-20 gap-10">
+          <div class="desktop:col-span-1 tablet:col-span-1 col-span-2 flex justify-center items-center">
+            <img
+              src={plant}
+              class="desktop:h-[500.45px] desktop:w-[640px] tablet:w-[280px] tablet:h-[218.95px] w-[240px] h-[187.67px]"
+              alt="plant"
+            ></img>
+          </div>
+          <div class="desktop:col-span-1 tablet:col-span-1 col-span-2 flex flex-col justify-center desktop:items-start tablet:items-start items-center desktop:px-5 tablet:px-5 px-10">
+            <div class="Nova Flat w-full desktop:text-[28px] tablet:text-[12px] text-[12px] desktop:text-left tablet:text-left text-center">
+              Monospace
+            </div>
+            <div class="font-extrabold w-full desktop:text-[44px] tablet:text-[20px] text-[18px] desktop:text-left tablet:text-left text-center">
+              盆器租借系統
+            </div>
+            <div class="mt-6 w-full text-[#9D9D9D] desktop:text-[20px] tablet:text-[14px] text-[14px]">
+              打造自動化系統，隨時檢測植物生長環境，讓你的植物安心生長。
+            </div>
+            <ul class="list-disc mt-6 pl-6  desktop:leading-10 text-gray-500  desktop:text-[20px] tablet:text-[14px] tablet:leading-7 text-[14px] leading-5">
+              <li>土壤溼度感測</li>
+              <li>光照強度感測</li>
+              <li>光照開關控制</li>
+              <li>預約盆器租借</li>
+              <li>租借盆器遞補通知</li>
+            </ul>
+          </div>
+        </div>
       </div>
+      {/* State */}
       <div id="state" class="w-full flex justify-center">
-        <State rents={user.rents} containerId={1} path={url}></State>
-      </div>
-      <h1
-        id="showPlant"
-        class=" text-center pt-20 font-extrabold text-[44px] tablet:text-[20px] phone:text-[18px] tablet:pt-0 phone:pt-10"
-      >
-        會員植物
-      </h1>
-      <div class="flex justify-center">
-        <div class="flex flex-row overflow-x-scroll overflow-y-hidden items-center w-full max-w-[1560px] h-[803px] mb-20 tablet:mb-10 text-center phone:mb-10">
-          {otherPlant.data
-            .filter((item) => item.plant !== null)
-            .map((item) => (
-              <Showplant key={item.container} data={item}></Showplant>
+        <div class="desktop:mt-[60px] tablet:mt-5 mt-4 w-full desktop:max-w-[1560px] tablet:max-w-[768px] max-w-[375px]">
+          <h1 class=" text-center font-extrabold text-[44px] tablet:text-[20px] phone:text-[18px]">
+            盆栽狀態
+          </h1>
+          {user.rents
+            .filter((x) => x.plant !== null)
+            .map((x) => (
+              <StatePlant key={x.id} rent={x} path={url}></StatePlant>
             ))}
         </div>
       </div>
-      <div class="w-full flex justify-center bg-[#F9FAFB] desktop:py-24 py-12">
+      {/* Other Plant */}
+      <div id="other" class="w-full flex justify-center">
+        <div class="desktop:mt-[60px] tablet:mt-5 mt-4 w-full desktop:max-w-[1560px] tablet:max-w-[768px] max-w-[375px]">
+          <h1
+            id="showPlant"
+            class=" text-center pt-20 font-extrabold text-[44px] tablet:text-[20px] phone:text-[18px] tablet:pt-0 phone:pt-10"
+          >
+            會員植物
+          </h1>
+          <div class="flex justify-center">
+            <div class="flex flex-row overflow-x-scroll overflow-y-hidden items-center w-full max-w-[1560px] h-[803px] mb-20 tablet:mb-10 text-center phone:mb-10">
+              {otherPlant.data
+                .filter((item) => item.plant !== null)
+                .map((item) => (
+                  <Showplant key={item.container} data={item}></Showplant>
+                ))}
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* Footer */}
+      <div
+        id="footer"
+        class="w-full flex justify-center bg-[#F9FAFB] desktop:py-24 py-12"
+      >
         <div class="flex flex-col items-center gap-6">
           <h1 class="font-extrabold desktop:text-[36px] tablet:text-[24px] text-[20px] tracking-widest">
             現在就租借你的盆器
