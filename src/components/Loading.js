ab2cf69f839efbe7753f09b0ada07c30440e09ce
jimchen5209 <jimchen5209@gmail.com>
2022-10-28 17:03:00 +0800

    fix: className on Loading component

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,7 +1,7 @@
 const Loading = () => (
   <div role="status">
     <svg
-      class="inline mr-2 w-10 h-10 text-gray-200 animate-spin fill-green-600"
+      className="inline mr-2 w-10 h-10 text-gray-200 animate-spin fill-green-600"
       viewBox="0 0 100 101"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
@@ -15,7 +15,7 @@ const Loading = () => (
         fill="currentFill"
       />
     </svg>
-    <span class="sr-only">Loading...</span>
+    <span className="sr-only">Loading...</span>
   </div>
 );
 
