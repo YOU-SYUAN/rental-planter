57d14a9291fdd34b46e68f1182d78d8b4c3846ee
jimchen5209 <jimchen5209@gmail.com>
2022-10-28 18:22:15 +0800

    refactor: login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -19,11 +19,12 @@ const Login = () => {
           navigate("./admin");
         }
       })
-      .catch(() => {});
+      .catch(() => { });
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  const login = () => {
+  const login = (e) => {
+    e.preventDefault();
     userLogin({ email: email.current.value, password: password.current.value })
       .then((response) => {
         if (response.status === 200) {
@@ -54,97 +55,79 @@ const Login = () => {
   };
 
   return (
-    <div className="App">
+    <div id="login" className="bg-cover bg-center w-screen h-screen flex justify-center items-center overflow-auto" style={{ backgroundImage: `url(${Background})` }}>
       <div
-        className="relative bg-cover bg-center"
-        style={{ backgroundImage: `url(${Background})`, height: "100vh" }}
+        className="flex flex-col w-full max-w-xl items-center justify-center p-4 desktop:gap-16 gap-8"
       >
-        <div className="top-[100px] w-full fixed h-[93px]"></div>
+        <div className="w-full flex flex-col gap-4">
+          <p className="text-center text-white font-['Nova_Flat'] desktop:text-[80px] tablet:text-[80px] text-[44px]">
+            Rental Planter
+          </p>
+          <p className="text-center text-white tracking-[.4em] desktop:text-[28px] tablet:text-[28px] text-[16px]">
+            用心照顧你的植物
+          </p>
+        </div>
 
-        <div
-          id="authentication-modal"
-          tabindex="-1"
-          className="overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 w-full tablet:inset-0 h-full tablet:h-full justify-center flex content-start "
-          aria-modal="true"
-          role="dialog"
-        >
-          <div className="relative p-4 w-fit h-full tablet:h-auto flex flex-col items-center justify-center">
-            <div className="w-full  phone:mt-[99.5px]">
-              <p className="text-center text-[80px] text-white font-['Nova_Flat'] tablet:text-[80px] phone:text-[44px] ">
-                Rental Planter
-              </p>
-              <p className="text-center text-[28px] mt-6 text-white tracking-[.40em] tablet:text-[28px] phone:text-[16px] phone:mt-6">
-                用心照顧你的植物
-              </p>
-            </div>
-
-            <div className="relative w-[480px] h-[520px] bg-white max-w-lg rounded-lg mt-[64px] shadow dark:bg-gray-700 py-12 phone:w-[343px] phone:h-[387px]">
-              <p className="text-[32px] text-center font-semibold phone:text-[24px]">
-                Monospace VIP
-              </p>
-              <div className="relative mt-[48px]">
-                <div className=" px-8 desktop:px-8">
-                  <form className="" action="#">
-                    <div>
-                      <label
-                        for="email"
-                        className="block mb-2 text-[20px] font-medium text-gray-900 dark:text-gray-300 phone:text-[14px]"
-                      >
-                        電子郵件
-                      </label>
-                      <input
-                        type="email"
-                        name="email"
-                        id="email"
-                        ref={email}
-                        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
-                        placeholder="輸入電子郵件"
-                        required=""
-                      />
-                    </div>
-                    <div className="mt-6">
-                      <label
-                        for="password"
-                        className="block mb-2 text-[20px] font-medium text-gray-900 dark:text-gray-300 phone:text-[14px]"
-                      >
-                        密碼
-                      </label>
-                      <input
-                        type="password"
-                        name="password"
-                        id="password"
-                        ref={password}
-                        placeholder="輸入密碼"
-                        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
-                        required=""
-                      />
-                    </div>
-                    <div className="grid grid-col-2 grid-flow-col space-between">
-                      <div className="">
-                        <label className="text-[#FF0000] ">{errorMsg}</label>
-                      </div>
-                      <div className=" mt-6 grid justify-items-end">
-                        <a href="./forgetPwd" className=" text-[#1C64F2] ">
-                          忘記密碼?
-                        </a>
-                      </div>
-                    </div>
-                    <button
-                      onClick={login}
-                      type="button"
-                      className="w-full mt-6 text-white bg-[#519E75] hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-[20px] px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 phone:mt-6"
-                    >
-                      登入
-                    </button>
-                  </form>
-                  <div className="flex flex-rox mt-4">
-                    <label className="text-gray-500">尚未註冊?</label>
-                    <a href="./register" className="text-[#1C64F2]">
-                      註冊
+        <div className="bg-white w-full max-w-lg rounded-lg shadow desktop:py-12 tablet:py-12 py-6 flex flex-col desktop:gap-12 tablet:gap-12 gap-6">
+          <p className="desktop:text-[32px] tablet:text-[32px] text-[24px] text-center font-semibold">
+            Monospace VIP
+          </p>
+          <div className="desktop:px-8 tablet:px-8 px-4 flex flex-col gap-4">
+            <form className="flex flex-col gap-6" onSubmit={login}>
+              <div className="flex flex-col gap-2">
+                <label
+                  htmlFor="email"
+                  className="font-medium text-gray-900 desktop:text-[20px] tablet:text-[20px] text-[18px]"
+                >
+                  電子郵件
+                </label>
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  ref={email}
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-full p-2.5"
+                  placeholder="輸入電子郵件"
+                  required
+                />
+              </div>
+              <div className="flex flex-col gap-2">
+                <label
+                  htmlFor="password"
+                  className="font-medium text-gray-900 desktop:text-[20px] tablet:text-[20px] text-[18px]"
+                >
+                  密碼
+                </label>
+                <input
+                  type="password"
+                  name="password"
+                  id="password"
+                  ref={password}
+                  placeholder="輸入密碼"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-full p-2.5"
+                  required
+                />
+                <div className="grid grid-col-2 grid-flow-col space-between">
+                  <label className="text-[#FF0000]">{errorMsg}</label>
+                  <div className="grid justify-items-end">
+                    <a href="./forgetPwd" className="desktop:text-[16px] tablet:text-[16px] text-[14px] text-[#1C64F2] ">
+                      忘記密碼?
                     </a>
                   </div>
                 </div>
               </div>
+              <button
+                type="submit"
+                className="w-full text-white bg-[#519E75] hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg desktop:text-[24px] text-[20px] px-5 py-2.5 text-center"
+              >
+                登入
+              </button>
+            </form>
+            <div className="flex desktop:text-[16px] tablet:text-[16px] text-[14px]">
+              <label className="text-gray-500">尚未註冊?</label>
+              <a href="./register" className="text-[#1C64F2]">
+                註冊
+              </a>
             </div>
           </div>
         </div>
